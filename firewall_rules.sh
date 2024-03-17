e7acf13e1e1203ea9d8686476ad2f93913c23e63
Orlando Filho <orlandossf04@gmail.com>
2024-03-17 20:03:07 -0300

    update

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+apt-get install sudo
+
+sudo iptables -F
+sudo iptables -X
+
 # Define a política padrão para DROP (bloquear tudo)
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
@@ -10,10 +15,15 @@ sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 
 # Permite o tráfego de resposta relacionado e estabelecido
+# sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Bloqueia o acesso a um site específico
 sudo iptables -A OUTPUT -p tcp -m string --string "Host: globo.com" --algo kmp --to 65535 -j DROP
+
+sudo iptables -A INPUT -p udp -d 67:68 --sport 67:68 -j ACCEPT
+sudo iptables -A INPUT -p udp -d 53 -j ACCEPT
+sudo iptables -A INPUT -p tcp -d 53 -j ACCEPT
 
 # Exibe as regras iptables
 sudo iptables -L -v
