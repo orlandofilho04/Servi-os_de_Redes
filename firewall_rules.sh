6ea02343d9ab4ee8d9cb1da8951612640a0a768b
Orlando Filho <orlandossf04@gmail.com>
2024-03-25 19:55:28 -0300

    update

--- firewall_rules.sh
+++ firewall_rules.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
-apt-get install sudo
+# Política padrão
+iptables -F
+iptables -X
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
 
-sudo iptables -F
-sudo iptables -X
+# Liberar loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
-# Define a política padrão para DROP (bloquear tudo)
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -P OUTPUT ACCEPT
+# Liberar conexões já estabelecidas
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
-# Permite tráfego local
-sudo iptables -A INPUT -i lo -j ACCEPT
-sudo iptables -A OUTPUT -o lo -j ACCEPT
+# Liberar conexões
+iptables -A INPUT -p udp -d 53 -j ACCEPT
+iptables -A INPUT -p tcp -d 53 -j ACCEPT
 
-# Permite o tráfego de resposta relacionado e estabelecido
-# sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# Bloqueia o acesso a um site específico
-sudo iptables -A OUTPUT -p tcp -m string --string "Host: globo.com" --algo kmp --to 65535 -j DROP
-
-sudo iptables -A INPUT -p udp -d 67:68 --sport 67:68 -j ACCEPT
-sudo iptables -A INPUT -p udp -d 53 -j ACCEPT
-sudo iptables -A INPUT -p tcp -d 53 -j ACCEPT
-
-# Exibe as regras iptables
-sudo iptables -L -v
-
-# Mantém o contêiner em execução (importante para contêineres baseados em Ubuntu)
-sudo tail -f /dev/null
+iptables -A INPUT -p udp --dport 67:68 -j ACCEPT
+iptables -L
